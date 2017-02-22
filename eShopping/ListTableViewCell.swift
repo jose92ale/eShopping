6237821786bb883434e971848bc38e2d2706386b
jose92ale <jose92ale@gmail.com>
2017-02-21 19:42:38 -0500

    Prototype Cell

---
+++ eShopping/ListTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ListTableViewCell.swift
+//  eShopping
+//
+//  Created by Jose Alejandro Apablaza on 2017-02-21.
+//  Copyright © 2017 Jose Alejandro Apablaza. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    
+    
+    @IBOutlet weak var nameLabel: UIView!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var quantitySpepper: UIStepper!
+    
+
+}
